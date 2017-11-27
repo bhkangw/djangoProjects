8dabb675551df3ba2cb5a51945a312dd5f66ac25
Brian Kang <bhkangw@gmail.com>
2017-11-26 18:19:50 -0800

    Added LikesBooks

---
+++ likes_books.py
@@ -0,0 +1,39 @@
+
+# For your assignment:
+
+
+# Create 3 different user accounts
+
+user1 = User.objects.create(first_name="Brian", last_name="Kang", email="bk@me.com")
+user2 = User.objects.create(first_name="Brian", last_name="Pang", email="bp@me.com")
+user3 = User.objects.create(first_name="Brian", last_name="Dang", email="bd@me.com")
+
+# Have the first user create 2 books.
+
+
+
+# Have the second user create 2 other books.
+
+
+# Have the third user create 2 other books.
+
+
+# Have the first user like the last book and the first book
+
+
+# Have the second user like the first book and the third book
+
+
+# Have the third user like all books
+
+
+# Display all users who like the first book
+
+
+# Display the user who uploaded the first book
+
+
+# Display all users who like the second book
+
+
+# Display the user who uploaded the second book
